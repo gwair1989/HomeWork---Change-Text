05b81af65894a95d529ec2540bfb4eedceb1ab9d
Alexandr Halypa <gwair1989@gmail.cm>
2021-05-31 22:43:47 +0300

    Initial Commit

---
+++ Change Text in Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Change Text in Code
+//
+//  Created by Aleksandr Khalupa on 31.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
